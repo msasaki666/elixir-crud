b9cf0c5721d85a39ef18ed1aea746b96df8c0655
Sasaki Motoaki <voyegesansretour1@gmail.com>
2021-08-31 12:15:00 +0000

    scaffoldのコマンドあったすごい mix phx.gen.html Posts Post posts title:string body:text mix ecto.migrate

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -17,6 +17,8 @@ defmodule AppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    resources "/posts", PostController
   end
 
   # Other scopes may use custom stacks.
